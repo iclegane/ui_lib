e7dc05e48bb081da663d458c73158126e3b5af53
iclegane <nekros77@gmail.com>
2024-08-21 14:38:57 +0300

    init

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { Modal } from "./ui/Modal"
+
+function App() {
+  return (
+    <>
+      <Modal/>
+    </> 
+  )
+}
+
+export default App
---
+++ src/ui/Modal/Modal.tsx
@@ -0,0 +1,3 @@
+export const Modal: React.FC = () => {
+    return <div className="text-3xl font-bold underline">12322</div>
+}
